8d159bd9b3e6ca628a82335d6fde60bde66cf0aa
Adrian <adrianjewell91@gmail.com>
2017-12-13 15:41:38 -0800

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize the Noteflight client API.
+  NFClient.init(function(info) {
+    console.log("Noteflight API is ready, version " + info.version);
+    
+    var options = {
+      width: 800,
+      height: 400
+    };
+    
+    var scoreView = new NFClient.ScoreView('score1', 'fcfd6d0bc0770f67cdbe1b8129456521fec090a0', options);
+    
+  });
+  
+});
