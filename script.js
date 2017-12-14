b352980bfb224334192671a218cac1424c9571b7
Adrian <adrianjewell91@gmail.com>
2017-12-13 16:15:00 -0800

    trying to call a function

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize the Noteflight client API.
   NFClient.init(function(info) {
     console.log("Noteflight API is ready, version " + info.version);
     
     var options = {
+      host: 'www.noteflight.com',
       width: 800,
       height: 400
     };
     
     var scoreView = new NFClient.ScoreView('score1', 'fcfd6d0bc0770f67cdbe1b8129456521fec090a0', options);
+    debugger
+    scoreView.addEventListener('editorReady', () => {
+      alert("anyonethere?");
+    });
     
   });
   
